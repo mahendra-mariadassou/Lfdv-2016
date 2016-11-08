397071e97a01bba6588c5f5f9f9dcb9d3e28d85e
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2016-11-08 16:14:08 +0100

    Mise à jour du html

---
+++ update_website.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Revisions/Revisions.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Revisions/
+rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Revisions/Revisions_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Revisions/
