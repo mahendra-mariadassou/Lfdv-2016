1a677b1665283f48498ae68fe3d0dcdb452355fe
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2016-12-06 10:56:20 +0100

    Intrduction à l'analyse

--- update_website.sh
+++ update_website.sh
@@ -3,3 +3,5 @@ rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Revisions/Revisions.
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Revisions/Revisions_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Revisions/
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Complexes/Complexes.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Complexes/
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Complexes/Complexes_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Complexes/
+rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus/
+rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus/
