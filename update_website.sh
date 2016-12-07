a56bc08bff73f19a482cf41961cad5266522cf79
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2016-12-07 21:51:14 +0100

    modification du script de synchronisation

--- update_website.sh
+++ update_website.sh
@@ -5,3 +5,6 @@ rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Complexes/Complexes.
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Complexes/Complexes_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Complexes/
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus/
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus/
+rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
+rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
+rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
