df3b8f13974d2fe3811d9ca0ed8ab0845595fafa
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2017-03-19 21:22:34 +0100

    Complément cours Calculus 3

--- update_website.sh
+++ update_website.sh
@@ -8,3 +8,6 @@ rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.pd
 rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
 rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
 rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
+rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_3/Calculus_3.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_3/
+rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_3/Calculus_3_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_3/
+rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_3/Calculus_3.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_3/
